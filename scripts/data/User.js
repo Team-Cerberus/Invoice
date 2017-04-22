88e673f166b7d8bd35dd42ab87ce85bcd3540d57
hankrum <st_kolev@mail.bg>
2017-04-22 18:54:45 +0300

    User

--- scripts/data/User.js
+++ scripts/data/User.js
@@ -1,11 +1,27 @@
-class User {
+class User {        //a lot TODO here
 
-    constructor (
+    constructor(
         userName,
         password,
         sellers,
         buyers
     ) {
+        this._userName = userName;
+        this._password = password;
+
+        if (sellers) {
+            this._sellers = sellers;
+        }
+        else {
+            this._sellers = [];
+        }
+        
+        if (buyers) {
+            this._buyers = buyers;
+        }
+        else {
+            this._buyers = [];
+        }
         
     }
 }
