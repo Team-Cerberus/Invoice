06f21e05f248de0efca470ed63304cbc2c978079
hankrum <st_kolev@mail.bg>
2017-04-22 17:34:32 +0300

    BankAccount

--- scripts/data/BankAccount.js
+++ scripts/data/BankAccount.js
@@ -1,6 +1,19 @@
 class BankAccount {
     constructor(iBan, bIC, bankName) {
-
+        this._iBan = iBan;
+        this._bIC = bIC;
+        this._bankName = bankName;
     }
-    
+
+    get iBan() {
+        return _iBan;
+    }
+
+    get bIC() {
+        return _bIC;
+    }
+
+    get bankName() {
+        return _bankName;
+    }
 }
