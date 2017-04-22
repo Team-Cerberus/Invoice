a00a5c3a392e55c71f26cca528704e04c772155c
hankrum <st_kolev@mail.bg>
2017-04-22 17:59:23 +0300

    InvoiceRow

--- scripts/data/InvoiceRow.js
+++ scripts/data/InvoiceRow.js
@@ -1,6 +1,6 @@
 class InvoiceRow {
 
-    constructor (
+    constructor(
         productNumber,
         productName,
         measuringUnit,
@@ -8,10 +8,51 @@ class InvoiceRow {
         unitPrice,
         VATValue,
     ) {
+        this._productNumber = productNumber;
+        this._productName = productName;
+        this._measuringUnit = measuringUnit;
+        this._quantity = quantity;
+        this._unitPrice = unitPrice;
 
+        this._unitPriceWithVAT += this.unitPrice * VATValue;
+        this._rowPriceNoVAT = this.quantity * this.unitPrice;
+        this._rowVAT = this._rowPriceNoVAT * VATValue;
+        this._rowPriceWithVAT = this._rowPriceNoVAT + this._rowVAT;
+    }
 
-        this._unitPriceWithVAT += unitPrice*VATValue;
-        // others to be added
+    get productNumber() {
+        return this._productNumber;
+    }
 
+    get productName() {
+        return this._productName;
+    }
+
+    get measuringUnit() {
+        return this._measuringUnit;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    get unitPrice(){
+        return this._unitPrice;
+    }
+
+    get unitPriceWithVAT() {
+        return this._unitPriceWithVAT;
+    }
+
+    get rowPriceNoVAT() {
+        return this._rowPriceNoVAT;
+    }
+
+    get rowVAT() {
+        return this._rowVAT;
+    }
+
+    get rowPriceWithVAT() {
+        return this._rowPriceWithVAT;
     }
 }
