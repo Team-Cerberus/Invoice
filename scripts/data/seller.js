8b03f54bf159900bf8e9a5a99a695116bcafa28e
hankrum <st_kolev@mail.bg>
2017-04-22 18:33:22 +0300

    Seller

--- scripts/data/seller.js
+++ scripts/data/seller.js
@@ -10,20 +10,47 @@ class Seller extends Entity {
         authorizedPerson,
         recipients,
         issuers,
-        bankAccount,
+        bankAccounts,
         invoices,
     ) {
         super(
-        name,
-        streetAddress,
-        city,
-        zIP,
-        idNumber,
-        vATNumber,
-        authorizedPerson,
-        recipients,
+            name,
+            streetAddress,
+            city,
+            zIP,
+            idNumber,
+            vATNumber,
+            authorizedPerson,
+            recipients,
         );
 
-        
+        if (issuers) {
+            this._issuers = issuers;
+        }
+        else {
+            this._issuers = [];
+            this._issuers.push(this.authorizedPerson);
+        }
+
+        this._bankAccounts = bankAccounts;
+
+        this._invoices = [];
+    }
+
+    get issuers() {
+        return this._issuers;
+    }
+
+    get bankAccounts() {
+        return this._bankAccounts;
+    }
+
+    get invoices() {
+        return this._invoices;
+    }
+
+    addInvoice(invoiceToAdd) {
+        //VALIDATION
+        this._invoices.push(invoiceToAdd);
     }
 }
