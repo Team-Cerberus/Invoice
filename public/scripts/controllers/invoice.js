f215ca350e177c83f298cb76db08efbe4d79bb87
nikolovdeyan <deyan.nikolov@gmail.com>
2017-05-05 14:19:14 +0300

    Invoice template responsive skeleton start

--- public/scripts/controllers/invoice.js
+++ public/scripts/controllers/invoice.js
@@ -6,31 +6,54 @@ import {
 const $appContainer = $('#app-container');
 
 const sampleData = {
-  "issuer": "Testing Issuer",
-  "receiver": "Testing Receiver",
-  "inv-number": "00000666",
-  "inv-date-issued": "02.01.2017",
-  "inv-date-sale": "01.01.2017",
-  "inv-sale-list": [{
-      "id": "59-023",
-      "item-name": "A test item name",
-      "item-unit": "no",
+  "document": {
+    "document-type": "invoice",   
+    "document-is-original": "original",
+    "document-number": "00000777",
+    "document-date-issued": "02.01.2017",
+    "document-date-sale": "01.01.2017", 
+    "document-place-sale": "Hobbiton, The Shire"
+  },
+  "issuer": {
+      "issuer-name": "Bilbo and Associates Ltd.",
+      "issuer-city": "Hobbiton",
+      "issuer-address": "15 Grassroute str, Hobbiton, The Shire",
+      "issuer-uic": "123456789",
+      "issuer-vat": "098765432",
+      "issuer-accountable-person": "Frodo Baggins"
+  },
+  "receiver": {
+    "receiver-name": "Magical Jewellery Unlimited Ltd.",
+    "receiver-city": "Moria",
+    "receiver-address": "15, Grand Hall, Moria",
+    "receiver-uic": "999888777",
+    "receiver-vat": "123123123",
+    "receiver-accountable-person": "Gandalf the Gray"
+  },
+  "invoice-item-list": [
+    {
+      "item-id": "59-001",
+      "item-name": "The One Ring",
+      "item-unit": "number",
       "item-quantity": "1",
-      "item-unit-price": "4.40"
+      "item-unit-price": "999.99",
+      "item-total-price": "999.99"
     },
     {
-      "id": "59-100",
-      "item-name": "Another test item name",
-      "item-unit": "no",
-      "item-quantity": "2",
-      "item-unit-price": "1.25"
+      "item-id": "59-021",
+      "item-name": "Sting, an unique sword",
+      "item-unit": "number",
+      "item-quantity": "1",
+      "item-unit-price": "72.50",
+      "item-total-price": "72.50"
     },
     {
-      "id": "55-777",
-      "item-name": "Yet another test item name",
-      "item-unit": "no",
-      "item-quantity": "5",
-      "item-unit-price": "0.70"
+      "item-id": "59-024",
+      "item-name": "Mithril chainmail",
+      "item-unit": "number",
+      "item-quantity": "1",
+      "item-unit-price": "122.49",
+      "item-total-price": "122.49"
     }
   ]
 };
