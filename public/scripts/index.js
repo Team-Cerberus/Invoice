d269983fde457434ab3c24c877e7b166cf6f66a4
nikolovdeyan <deyan.nikolov@gmail.com>
2017-05-06 13:40:15 +0300

    Fixed a login issue

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,15 +9,9 @@ const router = new Router();
 
 router
   .on('/', () => location.hash = '#/home')
-//  .on('/home', () => homeController.get())
-//  .on('/user/register', () => userController.register())
-//  .on('/user/login', () => userController.logIn())
-//  .on('/user/logout', () => userController.logOut())
-//  .on('/user/:username', () => userController.profile())
-//  .on('/about', () => aboutController.get());
   .on('/user/register', userController.register)
-  .on('/user/login', userController.login)
-  .on('/user/logout', userController.logout)
+  .on('/user/login', userController.logIn)
+  .on('/user/logout', userController.logOut)
   .on('/user/:username', userController.profile)
   .on('/home', homeController.get)
   .on('/about', aboutController.get)
